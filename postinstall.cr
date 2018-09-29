d68a4c0bdcab15887bc014f3b9737dadba121c15
chenkovsky <chenkov@yeah.net>
2018-09-29 16:31:47 +0800

    add postinstall

---
+++ postinstall.cr
@@ -0,0 +1,8 @@
+# `cd blis && ./configure auto && make && make install -e INSTALL_LIBDIR=../ext`
+os = if flag?(:darwin)
+       "darwin"
+     else
+       raise "Not Supported"
+     end
+
+`ln -s ext/libblis_#{os}.a ext/libblis.a`
