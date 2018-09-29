a07b04e91280789201ae8be4bac08a89723ea4df
chenkovsky <chenkov@yeah.net>
2018-09-29 16:35:55 +0800

    fix bug

--- postinstall.cr
+++ postinstall.cr
@@ -1,8 +1,8 @@
 # cd blis && ./configure auto && make && make install -e INSTALL_LIBDIR=../ext
-os = if flag?(:darwin)
-       "darwin"
-     else
-       raise "Not Supported"
-     end
+{% if flag?(:darwin) %}
+  os = "darwin"
+{% else %}
+  raise "Not Supported"
+{% end %}
 
 `ln -s ext/libblis_#{os}.a ext/libblis.a`
